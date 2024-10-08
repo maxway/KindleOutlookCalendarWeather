4a40b939aaebd1b05112e0a928818737cf38d151
max <maxway@gmail.com>
2024-10-08 21:51:57 +0800

    pylint: vika module

--- bin/googlesheet/vikasheet.py
+++ bin/googlesheet/vikasheet.py
@@ -1,20 +1,34 @@
+'''Module Vika API'''
 import requests
 
 class VikaSheet:
+    '''vika sheet simple API, fetch the data by sheet_id & token '''
     def __init__(self, sheet_id, token):
-        self.url = f"https://api.vika.cn/fusion/v1/datasheets/{sheet_id}/records?maxRecords=10"
+        self.url = f"https://api.vika.cn/fusion/v1/datasheets/{sheet_id}/records?maxRecords=15"
         self.headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {token}"
         }
 
     def get_records(self):
-        print('vika:' + self.url)
-        print(self.headers)
-        response = requests.get(self.url, headers=self.headers)
-        return response.json()
+        '''pull data from vika'''
+        try:
+            response = requests.get(self.url, headers=self.headers, timeout=10)  # 设置超时时间为10秒
+            response.raise_for_status()  # 检查请求是否成功
+            return response.json()
+        except requests.exceptions.Timeout:
+            print('The request timed out.')
+            return None
+        except requests.exceptions.HTTPError as err:
+            print(f'HTTP error occurred: {err}')
+            return None
+        except requests.exceptions.RequestException as err:
+            print(f'Error occurred: {err}')
+            return None
+
 
     def get_titles(self):
+        '''filter title col'''
         records = self.get_records()
         titles = []
         for record in records['data']['records']:
